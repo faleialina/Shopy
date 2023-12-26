cad71ad3567c7716876a30bdece741e587312e58
a.falei <august3@mail.ru>
2023-12-27 00:02:57 +0300

    Provider store/ShopyPage

--- client/src/page/ShopyPage/ShopyPage.jsx
+++ client/src/page/ShopyPage/ShopyPage.jsx
@@ -3,31 +3,18 @@ import { Select } from '@mantine/core';
 import { Link } from 'react-router-dom';
 import style from './style.module.scss'
 import Header from "../../companents/Header/Header";
-import array from '../../Storage/Storage.json'
+
 import Filters from '../../companents/Filters/Filters';
 import Search from '../../companents/Search/Search';
+import { useGetAllProductsQuery } from '../../service/serviceProduct';
 
 
 function ShopyPage() {
     // const [searchString, setSearchString] = useState('');
     // const [expression, setExpression] = useState({ category: 'default', priceFrom: '', priceTo: '' });
 
-    const result = array.map(elem => <Link to={`/shopy/${elem.id}`}><div className={style.item}>
-        <div className={style.img}></div>
-        <h2>{elem.header}</h2>
-        <p>{elem.model}</p>
-        <p>{elem.BodyType}</p>
-        <p>{elem.motor}</p>
-        <div className={style.flex}>
-            <p>price</p>
-            <p>{elem.price}</p>
-        </div>
-        <div className={style.flex}>
-            <Button variant="default">All details</Button>
-            <Button variant="filled">Add to Cart</Button>
-            {/* onClick={doAdd} */}
-        </div>
-    </div></Link>)
+    const { data } = useGetAllProductsQuery();
+   
 
 
     return (
@@ -40,13 +27,32 @@ function ShopyPage() {
 
                 <Filters />
                 {/* setExpression={setExpression}  */}
-                <div className={style.itemBox}>{result}</div>
+                <div className={style.itemBox}>
+                    {data?.map((elem, index) => (
+                        <div key={index} className={style.item}>
+                            <div className={style.img}></div>
+                            <h2>{elem.header}</h2>
+                            <div className={style.flex}>
+                                <p>price</p>
+                                <p>{elem.price}</p>
+                            </div>
+                            <div className={style.flex}>
+                                <Button variant="default"><Link to={`/shopy/${elem.id}`}>All details</Link></Button>
+                                <Button variant="filled">Add to Cart</Button>
+                                {/* onClick={doAdd} */}
+                            </div>
+                        </div>
+                    ))}
+                </div>
+
             </div>
 
         </div>
 
 
     )
+
+
 }
 
 export default ShopyPage;
